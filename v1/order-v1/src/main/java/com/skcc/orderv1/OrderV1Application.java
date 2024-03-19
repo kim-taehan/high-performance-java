511be70c020b99394388c86c257065e56a1d27a9
kimtaehan11 <kimtaehan11@gmail.com>
2024-03-19 16:52:42 +0900

    first commit

---
+++ v1/order-v1/src/main/java/com/skcc/orderv1/OrderV1Application.java
@@ -0,0 +1,13 @@
+package com.skcc.orderv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderV1Application.class, args);
+	}
+
+}
---
+++ v1/stock-v1/src/main/java/com/skcc/stockv1/StockV1Application.java
@@ -0,0 +1,13 @@
+package com.skcc.stockv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockV1Application.class, args);
+	}
+
+}
